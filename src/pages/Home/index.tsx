fbf141b0f9f3080ac3ebb69b43e52dc082a851d1
Thaysa Zooby <thaysa.zooby@gmail.com>
2023-05-23 16:45:27 -0300

    feat: Estilizacao - Tabela

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
-import { HomeWrapper } from "./styles";
+import { HomeWrapper, PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Home() {
     return <HomeWrapper>
@@ -9,5 +9,35 @@ export function Home() {
         <Summary />
 
         <SearchForm />
+
+        <TransactionsContainer>
+            <TransactionsTable>
+                <thead>
+                    <td>Descrição</td>
+                    <td>Preço</td>
+                    <td>Categoria</td>
+                    <td>Data</td>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td width="50%">Desenvolvimento de site</td>
+                        <td>
+                            <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+                        </td>
+                        <td>Venda</td>
+                        <td>13/04/2022</td>
+                    </tr>
+
+                    <tr>
+                        <td width="50%">Hamburguer</td>
+                        <td>
+                            <PriceHighLight variant="outcome">R$ 59,00</PriceHighLight>
+                        </td>
+                        <td>Venda</td>
+                        <td>10/04/2022</td>
+                    </tr>
+                </tbody>
+            </TransactionsTable>
+        </TransactionsContainer>
     </HomeWrapper>
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,7 +1,57 @@
 import styled from "styled-components";
 
 export const HomeWrapper = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 77em 1fr;
+  display: grid;
+  grid-template-columns: 1fr 77em 1fr;
 `;
 
+export const TransactionsContainer = styled.main`
+  grid-column: 2 / 3;
+
+  width: 100%;
+  max-width: 1120px;
+  margin: 1rem auto 0;
+  padding: 0 1.5rem;
+`;
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+
+  td {
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+
+  thead {
+    td {
+      padding: 1rem 2rem;
+    }
+  }
+
+  tbody {
+    td {
+      padding: 1.25rem 2rem;
+      background: ${(props) => props.theme["gray-500"]};
+    }
+  }
+`;
+
+interface PriceHighlightProps {
+  variant?: "income" | "outcome";
+}
+
+export const PriceHighLight = styled.span<PriceHighlightProps>`
+  color: ${(props) =>
+    props.variant === "income"
+      ? props.theme["green-500"]
+      : props.theme["red-500"]};
+`;
